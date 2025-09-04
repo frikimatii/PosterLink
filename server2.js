444b8a6c8dfb107ba09cf42215d4b4ea61e6bc41
Frikimatii7 <matibiencomodo@gmail.com>
2025-09-04 17:53:05 -0300

    server Actual 2

--- server2.js
+++ server2.js
@@ -33,7 +33,10 @@ const authLimiter = ratelimit({
 });
 
 // --- Conexión a MongoDB ---
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log("✅ MongoDB conectado"))
     .catch(err => {
         console.error("❌ Error conectando a MongoDB:", err.message);
